4f2a0274fad10330f3ac8b98e5d2e9f6d6c38305
Davi <daviddevdobrasil@gmail.com>
2024-11-20 19:13:09 -0300

    Update onclick.js

--- onclick.js
+++ onclick.js
@@ -1,5 +1,4 @@
 function addDivs(clickedDiv) {
-    // Recupera o elemento pai da div clicada
     const parentDiv = clickedDiv.parentNode;
 
 
@@ -7,7 +6,7 @@ function addDivs(clickedDiv) {
 
       const contatosMinimizar = parentDiv.querySelectorAll(".new-box-link");
       contatosMinimizar.forEach(link => link.remove());
-      return; // Sai da função após a remoção
+      return; 
     }
 
 
@@ -15,43 +14,29 @@ function addDivs(clickedDiv) {
       const link = document.createElement('a');
        
       link.classList.add('new-box-link');
-      
-      
-      if (i===1){
-        link.href = 'mailto:daviddevdobrasil@gmail.com?subject=Entrevista&body=Olá,%20gostaria%20de%20marcar%20uma%20entrevista%20com%20você.%20Tem%20algum%20horário%20disponível?';
-      } else if (i===2){
-        whatsappLink.href = 'https://wa.me/5581994702499?text=Olá,%20gostaria%20de%20marcar%20uma%20entrevista.%20Você%20tem%20algum%20horário%20disponível?';
-      }else if(i===3){
-        link.href = 'https://www.instagram.com/daviddevbrasil/profilecard/?igsh=MXAwZDYwd2N1N3BqNA==';
-      }
-
       const newDiv = document.createElement('div');
       newDiv.classList.add('new-box');
 
       const img = document.createElement('img');
-      if(i === 1){
-        img.src = './gmail.png';
-      }else if(i === 2){
-        img.src = './whatsapp.png';
-        
-      }else if(i === 3){
-        img.src = './insta.png';
-      }
-      img.classList.add('imgsconta');
       
       
-       // Exemplo de imagem
-      img.alt = 'Imagem ' + i;
-
       const text = document.createElement('span');
       
-      if (i === 1){
+      if (i===1){
+        link.href = 'mailto:daviddevdobrasil@gmail.com?subject=Entrevista&body=Olá,%20gostaria%20de%20marcar%20uma%20entrevista%20com%20você.%20Tem%20algum%20horário%20disponível?';
+        img.src = './gmail.png';
         text.textContent = 'daviddevdobrasil@gmail.com';
-      }else if (i === 2){
+      } else if (i===2){
+        link.href = 'https://wa.me/5581994702499?text=Olá,%20gostaria%20de%20marcar%20uma%20entrevista.%20Você%20tem%20algum%20horário%20disponível?';
+        img.src = './whatsapp.png';
         text.textContent = '+55 81 994702499';
-      }else if (i === 3){
+      }else if(i===3){
+        link.href = 'https://www.instagram.com/daviddevbrasil/profilecard/?igsh=MXAwZDYwd2N1N3BqNA==';
+        img.src = './insta.png';
         text.textContent = '@daviddev';
       }
+
+      img.classList.add('imgsconta');
       
       text.classList.add('textconta');
       
@@ -60,7 +45,6 @@ function addDivs(clickedDiv) {
       newDiv.appendChild(text);
       link.appendChild(newDiv);
 
-      // Insere o link ao lado da div clicada
       parentDiv.appendChild(link);
     }
   }
